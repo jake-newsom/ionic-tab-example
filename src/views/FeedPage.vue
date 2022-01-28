610b0c08a29f289420a09dae18d941ce4ea1a86e
Jake Newsom <jake@keepingcurrentmatters.com>
2022-01-28 10:58:29 -0500

    Showing popover not working

--- src/views/FeedPage.vue
+++ src/views/FeedPage.vue
@@ -11,7 +11,7 @@
           <ion-title size="large">Feed</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+<!--     
         <ion-button
           expand="block"
           :router-link="'/tabs/feed/blog'"
@@ -24,15 +24,36 @@
           :router-link="'/tabs/feed/script'"
         >
           View Script
-        </ion-button>
+        </ion-button> -->
+
+        
+      <ion-button id="showpopover">Show Popover</ion-button>
+      <ion-popover
+        mode="ios"
+        trigger="showpopover"
+      >
+        <ion-content>
+          <ion-list>
+            <ion-item button >
+              Share as Link
+            </ion-item>
+            <ion-item button>
+              Share as Video
+            </ion-item>
+          </ion-list>
+          </ion-content>
+      </ion-popover>
+
+      
     </ion-content>
+    
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonPopover, IonList, IonItem } from '@ionic/vue';
 
 export default  {
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton, IonPopover, IonList, IonItem }
 }
 </script>
